50c1aedb40286a9c9fb7534621844812c69b0438
Muthyam Uppari <your.email@example.com>
2026-02-13 19:21:48 +0530

    initial project push

---
+++ src/main/java/com/alpha/foodorbit/FoodorbitApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.foodorbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodorbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodorbitApplication.class, args);
+	}
+
+}
