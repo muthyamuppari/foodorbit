11c51235d768112d2b865da80492c6060c9fecff
Muthyam Uppari <muthyamuppari1212@gmail.com>
2026-02-14 09:44:59 +0530

    Add unique constraint to otp field in Order entity
    otp unique

--- src/main/java/com/alpha/foodorbit/entities/Order.java
+++ src/main/java/com/alpha/foodorbit/entities/Order.java
@@ -19,6 +19,7 @@ public class Order {
     private List<Item> items;
     private String pickupAddress;
     private String deliveryAddress;
+	@@Column(unique = true)
     private String otp;
     private DeliveryPartner deliveryPartner;
     private Payment payment;
