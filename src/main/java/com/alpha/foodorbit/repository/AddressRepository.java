b67329399dc8a7defd6218f5ec66f79ed0697585
Muthyam Uppari <muthyamuppari1212@gmail.com>
2026-02-17 10:18:50 +0530

    Refactor AddressRepository to clean up imports

--- src/main/java/com/alpha/foodorbit/repository/AddressRepository.java
+++ src/main/java/com/alpha/foodorbit/repository/AddressRepository.java
@@ -1,11 +1,5 @@
 package com.alpha.foodorbit.repository;
 
-import com.alpha.foodorbit.entities.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address,Integer> {
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,8 +9,7 @@ import com.alpha.foodorbit.entities.Address;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
-	
-	public Optional<Address> findByStreet(String street);
-	
+
+    Optional<Address> findByStreet(String street);
 
 }
