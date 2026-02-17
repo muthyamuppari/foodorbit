160765bd4604fe1c5342afa1639c5c668032fcf1
Muthyam Uppari <muthyamuppari1212@gmail.com>
2026-02-17 10:27:28 +0530

    Update RestaurantRepository.java

--- src/main/java/com/alpha/foodorbit/repository/RestaurantRepository.java
+++ src/main/java/com/alpha/foodorbit/repository/RestaurantRepository.java
@@ -1,13 +1,5 @@
 package com.alpha.foodorbit.repository;
 
-import com.alpha.foodorbit.entities.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RestaurantRepository extends JpaRepository<Restaurant,Integer> {
-
-    Restaurant findByMobno(long mobno);
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,6 +10,7 @@ import com.alpha.foodorbit.entities.Restaurant;
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
 
-	public Optional<Restaurant> findByMailid(String mailid);
-	
+    Optional<Restaurant> findByMailid(String mailid);
+
 }
+
