d17fe76f9e4968147d30f55d37b6970f3e121095
Muthyam Uppari <muthyamuppari1212@gmail.com>
2026-02-17 10:22:26 +0530

    Update DeliveryPartnerRepository.java

--- src/main/java/com/alpha/foodorbit/repository/DeliveryPartnerRepository.java
+++ src/main/java/com/alpha/foodorbit/repository/DeliveryPartnerRepository.java
@@ -1,13 +1,5 @@
 package com.alpha.foodorbit.repository;
 
-import com.alpha.foodorbit.entities.DeliveryPartner;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeliveryPartnerRepository extends JpaRepository<DeliveryPartner,Integer> {
-
-    DeliveryPartner findByMobno(long mobno);
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,7 +9,7 @@ import com.alpha.foodorbit.entities.DeliveryPartner;
 
 @Repository
 public interface DeliveryPartnerRepository extends JpaRepository<DeliveryPartner, Integer> {
-	
-	public Optional<DeliveryPartner>  findByVehicleNo(String vehicleNo);
+
+    Optional<DeliveryPartner> findByVehicleNo(String vehicleNo);
 
 }
