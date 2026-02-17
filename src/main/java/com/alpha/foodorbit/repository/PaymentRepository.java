3ac344d56bcb8fe20866a01ac4d7395d2ff2a009
Muthyam Uppari <muthyamuppari1212@gmail.com>
2026-02-17 10:26:10 +0530

    Refactor PaymentRepository interface structure

--- src/main/java/com/alpha/foodorbit/repository/PaymentRepository.java
+++ src/main/java/com/alpha/foodorbit/repository/PaymentRepository.java
@@ -1,9 +1,5 @@
 package com.alpha.foodorbit.repository;
 
-import com.alpha.foodorbit.entities.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment,Integer> {
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +7,5 @@ import com.alpha.foodorbit.entities.Payment;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-	
-       
 
 }
